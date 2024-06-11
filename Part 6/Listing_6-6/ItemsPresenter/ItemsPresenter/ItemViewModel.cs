956e3d72fe77e6b68e07d79008c6cb5d8a7b1413
s-novoselov <nsoft72@gmail.com>
2024-06-11 15:31:16 +0300

    Add files via upload

---
+++ Part 6/Listing_6-6/ItemsPresenter/ItemsPresenter/ItemViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemsPresenter
+{
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // fires notification if a property changes
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        // collection of PersonViewModel objects
+        public ObservableCollection<PersonViewModel> People { get; } =
+            new ObservableCollection<PersonViewModel>();
+
+        // number of people
+        public int NumberOfPeople => People.Count;
+
+        public ItemViewModel()
+        {
+            People.CollectionChanged += People_CollectionChanged;
+
+            People.Add(new PersonViewModel("Alex", "Ponomarev"));
+            People.Add(new PersonViewModel("Victor", "Shannon"));
+            People.Add(new PersonViewModel("Thom", "Watkins"));
+        }
+
+        // whenever collection changes, fire notification for possible updates
+        // of NumberOfPeople and CanRemoveLast properties.
+        private void People_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(NumberOfPeople));
+            OnPropertyChanged(nameof(CanRemoveLast));
+        }
+
+        // can remove last item only if collection has some items in it
+        public bool CanRemoveLast => NumberOfPeople > 0;
+
+        // remove last item of the collection
+        public void RemoveLast()
+        {
+            People.RemoveAt(NumberOfPeople - 1);
+        }
+    }
+}
---
+++ Part 6/Listing_6-6/ItemsPresenter/ItemsPresenter/PersonViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemsPresenter
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public PersonViewModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
