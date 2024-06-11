3ebc43ba47932f964915d1488a99d9b255a1a4b8
s-novoselov <nsoft72@gmail.com>
2024-06-11 15:22:01 +0300

    Add files via upload

---
+++ Part 5/Listing_5-2/AccessProperties/AccessProperties/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AccessProperties
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
