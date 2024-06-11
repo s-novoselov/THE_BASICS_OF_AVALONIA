2dd560105d005daf7f1ce61e37f5f86b5495ab80
s-novoselov <nsoft72@gmail.com>
2024-06-11 15:28:52 +0300

    Add files via upload

---
+++ Part 6/Listing_6-2/CustomRoutedEvent/CustomRoutedEvent/StaticRoutedEvents.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRoutedEvent
+{
+    public static class StaticRoutedEvents
+    {
+        /// <summary>
+        /// create the MyCustomRoutedEvent
+        /// </summary>
+        public static readonly RoutedEvent<RoutedEventArgs> MyCustomRoutedEvent =
+            RoutedEvent.Register<object, RoutedEventArgs>
+            (
+                "MyCustomRouted",
+                RoutingStrategies.Tunnel | RoutingStrategies.Bubble
+            );
+    }
+}
