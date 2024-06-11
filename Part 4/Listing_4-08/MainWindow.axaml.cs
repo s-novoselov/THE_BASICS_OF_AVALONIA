d608f1813d96992289069ca970d0a0e36b887272
s-novoselov <nsoft72@gmail.com>
2024-06-11 14:59:19 +0300

    Add files via upload

---
+++ Part 4/Listing_4-08/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace BindingAvalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Width = 320;
+            this.Height = 150;
+        }
+    }
+}
